25f963b9b9e86f8af64752ec470f9db482b390a5
SayantanRC <sayantan.kgp@gmail.com>
2018-07-01 02:19:11 +0530

    use queryDB extensively. updateDB revamped

--- DBOperation.js
+++ DBOperation.js
@@ -11,6 +11,9 @@ const ACTION_DEVICE_DELETE = "DB_DEVICE_DELETE";
 const ACTION_UNIT_CREATE = "DB_UNIT_CREATE";
 const ACTION_UNIT_UPDATE = "DB_UNIT_UPDATE";
 const ACTION_UNIT_DELETE = "DB_UNIT_DELETE";
+const ACTION_EMPLOYEE_CREATE = "DB_EMPLOYEE_CREATE";
+const ACTION_EMPLOYEE_UPDATE = "DB_EMPLOYEE_UPDATE";
+const ACTION_EMPLOYEE_DELETE = "DB_EMPLOYEE_DELETE";
 
 const RESULT_OK = "RESULT_OK";
 const RESULT_ERROR = "RESULT_ERROR";
@@ -21,6 +24,8 @@ const RESULT_DUPLICATE_ID = "RESULT_DUPLICATE_ID";
 const COLLECTION_NAME_DEVICE = "device_collection";
 const COLLECTION_NAME_UNIT = "unit_collection";
 const COLLECTION_NAME_EMPLOYEE = "employee_collection";
+
+const KEY_CHANGES = "changes";
 
 const KEY_DEVICE_ID = "DeviceID";
 const KEY_DEVICE_NAME = "DeviceName";
@@ -33,6 +38,7 @@ const KEY_EMPLOYEE_NAME = "EmployeeName";
 const KEY_EMPLOYEE_MOBILE = "MobileNo";
 const KEY_EMPLOYEE_EMAIL = "Email";
 const KEY_EMPLOYEE_ISACTIVE = "IsActive";
+const KEY_EMPLOYEE_ISADMIN = "IsAdmin";
 const KEY_EMPLOYEE_PASSWD = "Password";
 const KEY_EMPLOYEE_TOKEN = "token";
 
@@ -82,26 +88,17 @@ class DBOperation {
                 db.db(this.DBName).collection(COLLECTION_NAME_UNIT).createIndex(uUID, {unique: true});
 
                 const result = Joi.validate(jsonData, requiredSchema, {allowUnknown: ifAllowUnknown});
-                if (result.error) callback(actionName, {"result": RESULT_BAD_DATA, "response": result.error});
+
+                if (result.error) callback(actionName, {"result": RESULT_BAD_DATA, "response": result.error.details[0].message});
                 else {
+
                     let search_item = {};
                     search_item[uniqueIDName] = jsonData[uniqueIDName];
-                    db.db(this.DBName).collection(collectionName).find(search_item).toArray((err, res) => {
-                        if (err) {
-                            db.close();
-                            callback(actionName, {
-                                "result": RESULT_ERROR,
-                                "response": "Duplicate validation error: " + err
-                            });
-                        }
-                        else if (res.length !== 0) {
-                            db.close();
-                            callback(actionName, {
-                                "result": RESULT_DUPLICATE_ID,
-                                "response": `${uniqueIDName}: ${jsonData[uniqueIDName]}`
-                            });
-                        }
-                        else {
+
+                    this.queryDB(collectionName, search_item, (request, response) => {
+
+                        if (response.result === RESULT_NO_SUCH_DEVICE){
+
                             db.db(this.DBName).collection(collectionName).insert(jsonData, (err, res) => {
                                 db.close();
                                 if (err) {
@@ -114,7 +111,25 @@ class DBOperation {
                                     callback(actionName, {"result": RESULT_OK, "response": res})
                                 }
                             })
+
                         }
+
+                        else if (response.result === RESULT_OK){
+
+                            callback(actionName, {
+                                "result": RESULT_DUPLICATE_ID,
+                                "response": `${uniqueIDName}: ${jsonData[uniqueIDName]}`
+                            });
+
+                        }
+
+                        else {
+                            callback(actionName, {
+                                "result": RESULT_ERROR,
+                                "response": "Validation error " + response.response
+                            })
+                        }
+
                     });
                 }
             }
@@ -126,44 +141,103 @@ class DBOperation {
 
         mongoConnector.onMongoConnect(this.DBUrl, callback, (db) => {
             if (db) {
-                const result = Joi.validate(jsonData, requiredSchema, {allowUnknown: ifAllowUnknown});
-                if (result.error) callback(actionName, {"result": RESULT_BAD_DATA, "response": result.error});
+
+                let uDID = {}, uUID = {};
+                uDID[KEY_DEVICE_ID] = 1;
+                uUID[KEY_UNIT_ID] = 1;
+                db.db(this.DBName).collection(COLLECTION_NAME_DEVICE).createIndex(uDID, {unique: true});
+                db.db(this.DBName).collection(COLLECTION_NAME_UNIT).createIndex(uUID, {unique: true});
+
+                const preSchema = {};
+                preSchema[uniqueIDName] = Joi.required();
+                preSchema[KEY_CHANGES] = Joi.required();
+
+                let JoiResult = Joi.validate(jsonData, preSchema);
+
+                if (JoiResult.error) callback(actionName, {"result": RESULT_BAD_DATA, "response": JoiResult.error.details[0].message});
                 else {
-                    let search_item = {};
-                    search_item[uniqueIDName] = jsonData[uniqueIDName];
-                    db.db(this.DBName).collection(collectionName).find(search_item).toArray((err, res) => {
-                        if (err) {
-                            db.close();
-                            callback(actionName, {"result": RESULT_ERROR, "response": "Validation error: " + err});
-                        }
-                        else if (res.length !== 0) {
-                            let combined_doc = res[0];
-                            for (let key1 in jsonData) {
-                                combined_doc[key1] = jsonData[key1];
+
+                    let search_original_id = {};
+                    search_original_id[uniqueIDName] = jsonData[uniqueIDName];
+
+                    this.queryDB(collectionName, search_original_id, (request, response1) => {
+
+                        if (response1.result === RESULT_OK){
+
+                            JoiResult = Joi.validate(jsonData[KEY_CHANGES], requiredSchema, {allowUnknown: ifAllowUnknown});
+
+                            if (JoiResult.error) callback(actionName, {"result": RESULT_BAD_DATA, "response": JoiResult.error.details[0].message});
+                            else {
+
+                                let search_updated_id = {};
+                                search_updated_id[uniqueIDName] = jsonData[KEY_CHANGES][uniqueIDName];
+
+                                this.queryDB(collectionName, search_updated_id, (request, response2) => {
+
+                                    if (response2.result === RESULT_NO_SUCH_DEVICE) {
+
+                                        let combined_doc = response1.response[0];
+                                        for (let key1 in jsonData[KEY_CHANGES]) {
+                                            combined_doc[key1] = jsonData[KEY_CHANGES][key1];
+                                        }
+                                        let updated_doc = {};
+                                        for (let key2 in combined_doc) {
+                                            if (combined_doc[key2])
+                                                updated_doc[key2] = combined_doc[key2];
+                                        }
+
+                                        db.db(this.DBName).collection(collectionName).update(search_original_id, updated_doc, (err, res) => {
+
+                                            db.close();
+                                            if (err) {
+                                                callback(actionName, {
+                                                    "result": RESULT_ERROR,
+                                                    "response": "Update error: " + err
+                                                });
+                                            }
+                                            else callback(actionName, {"result": RESULT_OK, "response": res});
+                                        })
+
+                                    }
+
+                                    else if (response2.result === RESULT_OK) {
+                                        db.close();
+                                        callback(actionName, {
+                                            "result": RESULT_DUPLICATE_ID,
+                                            "response": `Changed ID ${updated_doc[uniqueIDName]} already exists.`
+                                        });
+                                    }
+
+                                    else {
+                                        db.close();
+                                        callback(actionName, {
+                                            "result": RESULT_ERROR,
+                                            "response": "Updated validation error " + response2.response
+                                        })
+                                    }
+
+                                })
                             }
-                            let updated_doc = {};
-                            for (let key2 in combined_doc) {
-                                if (combined_doc[key2])
-                                    updated_doc[key2] = combined_doc[key2];
-                            }
-                            db.db(this.DBName).collection(collectionName).update(search_item, updated_doc, (err, res) => {
-                                db.close();
-                                if (err) {
-                                    callback(actionName, {
-                                        "result": RESULT_ERROR,
-                                        "response": "Update error: " + err
-                                    });
-                                }
-                                else callback(actionName, {"result": RESULT_OK, "response": res});
-                            })
+
                         }
-                        else {
-                            db.close();
+
+                        else if (response1.result === RESULT_NO_SUCH_DEVICE){
+
                             callback(actionName, {
                                 "result": RESULT_NO_SUCH_DEVICE,
                                 "response": `${uniqueIDName} : ${jsonData[uniqueIDName]}`
                             });
+
                         }
+
+                        else {
+                            callback(actionName, {
+                                "result": RESULT_ERROR,
+                                "response": "Validation error " + response1.response
+                            })
+                        }
+
+
                     });
                 }
             }
@@ -172,24 +246,25 @@ class DBOperation {
 
     }
 
-    deleteFromDB(jsonData, requiredSchema, collectionName, uniqueIDName, actionName, callback){
+    deleteFromDB(jsonData, collectionName, uniqueIDName, actionName, callback){
 
         mongoConnector.onMongoConnect(this.DBUrl, callback, (db) => {
             if (db) {
+
+                const requiredSchema = {};
+                requiredSchema[uniqueIDName] = Joi.required();
                 const result = Joi.validate(jsonData, requiredSchema);
-                if (result.error) callback(actionName, {"result": RESULT_BAD_DATA, "response": result.error});
+
+                if (result.error) callback(actionName, {"result": RESULT_BAD_DATA, "response": result.error.details[0].message});
                 else {
+
                     let search_item = {};
                     search_item[uniqueIDName] = jsonData[uniqueIDName];
-                    db.db(this.DBName).collection(collectionName).find(search_item).toArray((err, res) => {
-                        if (err) {
-                            db.close();
-                            callback(actionName, {
-                                "result": RESULT_ERROR,
-                                "response": "Validation error: " + err
-                            });
-                        }
-                        else if (res.length !== 0) {
+
+                    this.queryDB(collectionName, search_item, (request, response) => {
+
+                        if (response.result === RESULT_OK){
+
                             db.db(this.DBName).collection(collectionName).remove(search_item, (err, res) => {
                                 db.close();
                                 if (err) callback(actionName, {
@@ -198,14 +273,25 @@ class DBOperation {
                                 });
                                 else callback(actionName, {"result": RESULT_OK, "response": res});
                             });
+
                         }
-                        else {
-                            db.close();
+
+                        else if (response.result === RESULT_NO_SUCH_DEVICE){
+
                             callback(actionName, {
                                 "result": RESULT_NO_SUCH_DEVICE,
                                 "response": `${uniqueIDName} : ${jsonData[uniqueIDName]}`
                             });
+
                         }
+
+                        else {
+                            callback(actionName, {
+                                "result": RESULT_ERROR,
+                                "response": "Validation error " + response.response
+                            })
+                        }
+
                     });
                 }
             }
@@ -222,19 +308,16 @@ class DBOperation {
         this.addToDB(jsonDeviceData, requiredSchema, true, COLLECTION_NAME_DEVICE, KEY_DEVICE_ID, ACTION_DEVICE_CREATE, callback);
     }
 
-    updateDevice(jsonData, callback) {
+    updateDevice(jsonDeviceData, callback) {
         const requiredSchema = {};
-        requiredSchema[KEY_DEVICE_ID] = Joi.string().min(3).required();
+        requiredSchema[KEY_DEVICE_ID] = Joi.string().min(3);
         requiredSchema[KEY_DEVICE_NAME] = Joi.string().min(3);
 
-        this.updateDB(jsonData, requiredSchema, true, COLLECTION_NAME_DEVICE, KEY_DEVICE_ID, ACTION_DEVICE_UPDATE, callback);
+        this.updateDB(jsonDeviceData, requiredSchema, true, COLLECTION_NAME_DEVICE, KEY_DEVICE_ID, ACTION_DEVICE_UPDATE, callback);
     }
 
-    deleteDevice(jsonData, callback) {
-        const requiredSchema = {};
-        requiredSchema[KEY_DEVICE_ID] = Joi.string().min(3).required();
-
-        this.deleteFromDB(jsonData, requiredSchema, COLLECTION_NAME_DEVICE, KEY_DEVICE_ID, ACTION_DEVICE_DELETE, callback);
+    deleteDevice(jsonDeviceData, callback) {
+        this.deleteFromDB(jsonDeviceData, COLLECTION_NAME_DEVICE, KEY_DEVICE_ID, ACTION_DEVICE_DELETE, callback);
     }
 
     addUnit(jsonUnitData, callback) {
@@ -247,7 +330,8 @@ class DBOperation {
                 let requiredSchema = {};
                 requiredSchema[KEY_UNIT_ID] = Joi.string().min(3).required();
                 requiredSchema[KEY_DEVICE_ID] = Joi.string().min(3).required();
-                requiredSchema[KEY_EMPLOYEE_REGISTRATION_ID] = Joi.string().required();
+                requiredSchema[KEY_EMPLOYEE_REGISTRATION_ID] = Joi.any();
+                jsonUnitData[KEY_EMPLOYEE_REGISTRATION_ID] = "none";
                 this.addToDB(jsonUnitData, requiredSchema, false, COLLECTION_NAME_UNIT, KEY_UNIT_ID, ACTION_UNIT_CREATE, callback);
             }
             else {
@@ -265,7 +349,7 @@ class DBOperation {
 
             if (response.result === RESULT_OK){
                 let requiredSchema = {};
-                requiredSchema[KEY_UNIT_ID] = Joi.string().min(3).required();
+                requiredSchema[KEY_UNIT_ID] = Joi.string().min(3);
                 requiredSchema[KEY_DEVICE_ID] = Joi.string().min(3);
                 requiredSchema[KEY_EMPLOYEE_REGISTRATION_ID] = Joi.string();
                 this.updateDB(jsonUnitData, requiredSchema, false, COLLECTION_NAME_UNIT, KEY_UNIT_ID, ACTION_UNIT_UPDATE, callback);
@@ -277,11 +361,37 @@ class DBOperation {
         });
     }
 
-    deleteUnit(jsonData, callback){
-        const requiredSchema = {};
-        requiredSchema[KEY_UNIT_ID] = Joi.string().min(3).required();
+    deleteUnit(jsonUnitData, callback){
+        this.deleteFromDB(jsonUnitData, COLLECTION_NAME_UNIT, KEY_UNIT_ID, ACTION_UNIT_DELETE, callback);
+    }
 
-        this.deleteFromDB(jsonData, requiredSchema, COLLECTION_NAME_UNIT, KEY_UNIT_ID, ACTION_UNIT_DELETE, callback);
+    addEmployee(jsonEmployeeData, callback) {
+
+        let requiredSchema = {};
+        requiredSchema[KEY_EMPLOYEE_NAME] = Joi.string().min(3).required();
+        requiredSchema[KEY_EMPLOYEE_ID] = Joi.string().min(3).required();
+        requiredSchema[KEY_EMPLOYEE_EMAIL] = Joi.string().email().required();
+        requiredSchema[KEY_EMPLOYEE_MOBILE] = Joi.number().integer().min(10).required();
+        requiredSchema[KEY_EMPLOYEE_PASSWD] = Joi.string().min(6).required();
+        requiredSchema[KEY_EMPLOYEE_ISADMIN] = Joi.boolean().required();
+
+        this.addToDB(jsonEmployeeData, requiredSchema, true, COLLECTION_NAME_EMPLOYEE, KEY_EMPLOYEE_ID, ACTION_EMPLOYEE_CREATE, callback);
+    }
+
+    updateEmployee(jsonEmployeeData, callback) {
+        const requiredSchema = {};
+        requiredSchema[KEY_EMPLOYEE_NAME] = Joi.string().min(3);
+        requiredSchema[KEY_EMPLOYEE_ID] = Joi.string().min(3);
+        requiredSchema[KEY_EMPLOYEE_EMAIL] = Joi.string().email();
+        requiredSchema[KEY_EMPLOYEE_MOBILE] = Joi.number().integer().min(10);
+        requiredSchema[KEY_EMPLOYEE_PASSWD] = Joi.string().min(6);
+        requiredSchema[KEY_EMPLOYEE_ISADMIN] = Joi.boolean();
+
+        this.updateDB(jsonEmployeeData, requiredSchema, true, COLLECTION_NAME_EMPLOYEE, KEY_EMPLOYEE_ID, ACTION_EMPLOYEE_UPDATE, callback);
+    }
+
+    deleteEmployee(jsonEmployeeData, callback){
+        this.deleteFromDB(jsonEmployeeData, COLLECTION_NAME_EMPLOYEE, KEY_EMPLOYEE_ID, ACTION_EMPLOYEE_DELETE, callback);
     }
 
 }

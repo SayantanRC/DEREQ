312f948c7313396cdf3866504266a3bc49f4b946
SayantanRC <sayantan.kgp@gmail.com>
2018-07-10 21:23:02 +0530

    code commented in MongoConnector.js

--- MongoConnector.js
+++ MongoConnector.js
@@ -1,18 +1,36 @@
+
+//dependencies
 const mongoClient = require('mongodb').MongoClient;
 const DBOperation = require('./DBOperation');
 
 class MongoConnector {
+
+    //connect to mongo db using mongo db client
     onMongoConnect(url, parentCallback, DBOpCallback){
+
+        //parentCallback -> goes directly to the http response
+        //DBOpCallback -> goes to the function which requested connection to the database
+
         mongoClient.connect(url, (err, db) => {
+
+            //after connection is complete
+
             if (err) {
+
+                //in case of error
+                //directly send error to http response
                 if (parentCallback)
                 parentCallback(DBOperation.ACTION_DB_OPEN, {"result": DBOperation.RESULT_ERROR, "response": err.toString()});
             }
             else {
+
+                //if connection is successful
+                //send the database object
                 DBOpCallback(db);
             }
         })
     }
 }
 
+//export the MongoConnector class
 module.exports.MongoConnector = MongoConnector;
